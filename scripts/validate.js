1e26789b453d8a62484860fd2735523fae8f4e8b
tatsenko-m <tatsenko.m@yandex.ru>
2023-02-06 22:07:12 +0300

    feat: добавить функцию - переключатель состояния кнопки  submit

--- scripts/validate.js
+++ scripts/validate.js
@@ -2,11 +2,16 @@ const formValidationConfig = {
   formSelector: '.popup__form',
   inputSelector: '.popup__item',
   submitButtonSelector: '.popup__save-button',
-  inactiveButtonClass: 'popup__save-button_disabled', // нужно создать класс
+  inactiveButtonClass: 'popup__save-button_disabled',
   inputErrorClass: 'popup__item_type_error',
   errorClass: 'popup__error_visible'
 };
 
+function toggleSubmitButton(form, config) {
+  const buttonSubmit = form.querySelector(config.submitButtonSelector);
+  buttonSubmit.disabled = !form.checkValidity();
+  buttonSubmit.classList.toggle(config.inactiveButtonClass, !form.checkValidity());
+}
 
 function enableValidation(config) {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
@@ -19,3 +24,7 @@ function enableValidation(config) {
 
   });
 }
+
+
+
+toggleSubmitButton(cardForm, formValidationConfig);
