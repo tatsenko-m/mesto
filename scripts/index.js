480b17d3cb05eab09c6bfb120727753509850ce4
tatsenko-m <tatsenko.m@yandex.ru>
2023-03-02 18:01:52 +0300

    refactor: вынести массив начальных карточек в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,7 @@
+import initialCards from './cards.js';
 import { openPopup, closePopup } from './common.js';
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-
-// Данные шести исходных карточек
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // Объект настроек валидации
 const formValidationConfig = {
