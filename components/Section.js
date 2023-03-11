437fab12d8adac82d6f8cafa102f312ca5b09058
tatsenko-m <tatsenko.m@yandex.ru>
2023-03-11 21:12:40 +0300

    feat: написать класс Section

--- components/Section.js
+++ components/Section.js
@@ -1,6 +1,24 @@
 class Section {
   constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._container = document.querySelector(containerSelector);
+    this._renderer = renderer;
+  }
 
+  addItem(element) {
+    this._container.append(element);
+  }
+
+  clear() {
+    this._container.innerHTML = '';
+  }
+
+  renderItems() {
+    this.clear();
+
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
   }
 }
 
