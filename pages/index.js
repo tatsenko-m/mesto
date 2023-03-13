cd1cba8c03a75cb7288023a45b7cd20a93277490
tatsenko-m <tatsenko.m@yandex.ru>
2023-03-13 16:57:08 +0300

    feat: создать экземпляр класса popupWithImage и реализовать с помощью него открытие попапов изображений

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ import Section from '../components/Section.js';
 import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from '../components/UserInfo.js';
 
 const cardPopupElement = document.querySelector('.popup_act_add-card');
 const profilePopupOpenButtonElement = document.querySelector('.profile__edit-button');
@@ -26,6 +27,8 @@ const profileFormInputList = Array.from(profileForm.querySelectorAll('.popup__it
 const titleInput = cardForm.querySelector('.popup__item_type_title');
 const linkInput = cardForm.querySelector('.popup__item_type_link');
 const cardListSelector = '.gallery__list';
+const popupWithImageSelector = '.popup_act_open-img';
+
 const profileFormValidator = new FormValidator(formValidationConfig, profileForm);
 const cardFormValidator = new FormValidator(formValidationConfig, cardForm);
 
@@ -37,15 +40,21 @@ function openProfilePopup() {
   });
 }
 
+const popupWithImage = new PopupWithImage(popupWithImageSelector);
+
 const cardList = new Section({ items: initialCards, renderer: ({ name, link }) => {
-  const card = new Card({ name, link }, '#card-template');
+  const card = new Card({ name, link }, '#card-template', (name, link) => {
+    popupWithImage.open(name, link);
+  });
   const cardElement = card.createCard();
   cardList.addItem(cardElement);
 } }, cardListSelector);
 
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
-  const userCard = new Card({ name: titleInput.value, link: linkInput.value}, '#card-template');
+  const userCard = new Card({ name: titleInput.value, link: linkInput.value}, '#card-template', (name, link) => {
+    popupWithImage.open(name, link);
+  });
   const userCardElement = userCard.createCard();
   cardList.addItem(userCardElement);
   cardForm.reset();
