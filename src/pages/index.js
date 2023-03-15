e8c044b24228accb7f8786dac457cf3e392d8bbf
tatsenko-m <tatsenko.m@yandex.ru>
2023-03-16 01:35:01 +0300

    fix: вынести логику создания карточки в функцию createCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,38 +8,34 @@ import {
   popupWithImageSelector,
   popupWithEditProfileFormSelector,
   popupWithAddCardFormSelector,
-  cardTemplateId,
   profileElementSelectors,
   formValidators
 } from '../utils/constants.js';
-import Card from '../components/Card.js';
 import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
-import { enableValidation } from '../utils/utils.js';
+import { enableValidation, createCard } from '../utils/utils.js';
 
 const userInfo = new UserInfo(profileElementSelectors);
+
 const popupWithEditProfileForm = new PopupWithForm(popupWithEditProfileFormSelector, (data) => {
   userInfo.setUserInfo(data.name, data.about);
   popupWithEditProfileForm.close();
 });
+
 const popupWithImage = new PopupWithImage(popupWithImageSelector);
+
+const cardList = new Section({ items: initialCards, renderer: ({ name, link }) => {
+  const cardElement = createCard({ name, link }, popupWithImage);
+  cardList.addItem(cardElement);
+} }, cardListSelector);
+
 const popupWithAddCardForm = new PopupWithForm(popupWithAddCardFormSelector, (data) => {
-  const userCard = new Card({ name: data.title, link: data.link}, cardTemplateId, (name, link) => {
-    popupWithImage.open(name, link);
-  });
-  const userCardElement = userCard.createCard();
+  const userCardElement = createCard({ name: data.title, link: data.link}, popupWithImage);
   cardList.addItem(userCardElement);
   popupWithAddCardForm.close();
 });
-const cardList = new Section({ items: initialCards, renderer: ({ name, link }) => {
-  const card = new Card({ name, link }, cardTemplateId, (name, link) => {
-    popupWithImage.open(name, link);
-  });
-  const cardElement = card.createCard();
-  cardList.addItem(cardElement);
-} }, cardListSelector);
 
 popupWithEditProfileForm.setEventListeners();
 popupWithImage.setEventListeners();
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
-import { formValidators } from './constants.js';
+import { formValidators, cardTemplateId } from './constants.js';
 import FormValidator from '../components/FormValidator.js';
+import Card from '../components/Card.js';
 
 export const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector))
@@ -9,4 +10,12 @@ export const enableValidation = (config) => {
     formValidators[formName] = validator;
     validator.enableValidation();
   });
+};
+
+export const createCard = ({ name, link }, popupWithImageInstance) => {
+  const card = new Card({ name, link }, cardTemplateId, (name, link) => {
+    popupWithImageInstance.open(name, link);
+  });
+  const cardElement = card.createCard();
+  return cardElement;
 };
