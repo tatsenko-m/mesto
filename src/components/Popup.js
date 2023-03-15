f4075bc2d977d384034adec8010f7cf352c4a921
tatsenko-m <tatsenko.m@yandex.ru>
2023-03-15 23:18:58 +0300

    fix: исправить удаление обработчика Esc и заменить событие крестика на click

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,16 +2,17 @@ class Popup {
   constructor(popupSelector) {
     this._popupSelector = popupSelector;
     this._popupElement = document.querySelector(this._popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popupElement.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(event) {
@@ -22,7 +23,7 @@ class Popup {
 
   setEventListeners() {
     this._closeButtonElement = this._popupElement.querySelector('.popup__close-button');
-    this._closeButtonElement.addEventListener('mousedown', this.close.bind(this));
+    this._closeButtonElement.addEventListener('click', this.close.bind(this));
     this._popupElement.addEventListener('mousedown', (evt) => {
       if (evt.target.classList.contains('popup_opened')) {
         this.close();
